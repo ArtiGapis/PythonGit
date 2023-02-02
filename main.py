15f2ecd8503518681aa5e40e9b894806147ccf72
ArtiGapis <arturas.danilevicius@gmail.com>
2023-02-02 18:38:54 +0200

    Creating full project structure

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello world')
+import logging
+
+# logging.DEBUG is a constant, not the same as logging.debug() function
+# ... if you are running this in collab, you need to constantly restart the environment
+# ... to see the effects of log level changes
+logging.basicConfig(filename='logs/main.log', level=logging.INFO)
+
+def add(i, j):
+  return i + j
+
+def multiply(i, j):
+  return i * j
+
+res_add = add(3, 2)
+res_multiply = multiply(3, 2)
+
+logging.info('{} : {}'.format('Addition result', res_add))
+logging.info('{} : {}'.format('Multiplication result', res_multiply))
